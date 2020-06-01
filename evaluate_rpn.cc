c947e996c6edc364c20c244462b66dc500cc986d
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-05-31 18:11:09 -0700

    added solution

--- evaluate_rpn.cc
+++ evaluate_rpn.cc
@@ -1,10 +1,39 @@
 #include <string>
-
+#include <stack>
+#include <sstream>
 #include "test_framework/generic_test.h"
+
 using std::string;
+using std::stack;
+using std::stringstream;
 int Evaluate(const string& expression) {
   // TODO - you fill in here.
-  return 0;
+  const char delimiter = ',';
+  stack<int> intermediate_result;
+  stringstream ss(expression);
+  string token;
+  while(getline(ss, token, delimiter)){
+    if(token == "+" || token == "-" || token == "*" || token == "/"){
+        int y = intermediate_result.top();
+        intermediate_result.pop();
+        int x = intermediate_result.top();
+        intermediate_result.pop();
+
+        switch(token[0]){
+          case '+': intermediate_result.push(x + y);
+              break;
+          case '-': intermediate_result.push(x - y);
+              break;
+            case '*': intermediate_result.push(x * y);
+            break;
+            case '/': intermediate_result.push(x / y);
+            break;
+        }
+    } else {
+        intermediate_result.push(std::stoi(token));
+    }
+  }
+  return intermediate_result.top();
 }
 
 int main(int argc, char* argv[]) {
