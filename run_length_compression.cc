4a8972aaec654d0ba5e6797173de8d0bb839e009
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-02 11:19:14 -0700

    added solution

--- run_length_compression.cc
+++ run_length_compression.cc
@@ -4,13 +4,33 @@
 #include "test_framework/test_failure.h"
 using std::string;
 string Decoding(const string &s) {
-  // TODO - you fill in here.
-  return "";
+    string result;
+    int count = 0;
+    for(int i = 0;i < s.size();++i){
+        if(isdigit(s[i])){
+            count = count * 10 + (s[i] - '0');
+        } else{
+            result.append(count, s[i]);
+            count = 0;
+        }
+    }
+    return result;
 }
+
 string Encoding(const string &s) {
-  // TODO - you fill in here.
-  return "";
+    string result;
+    int count = 1;
+    for(int i = 1;i <= s.size();++i){
+        if(i == s.size() || s[i] != s[i - 1]){
+            result += std::to_string(count) + s[i - 1];
+            count = 1;
+        } else {
+            count++;
+        }
+    }
+    return result;
 }
+
 void RleTester(const string &encoded, const string &decoded) {
   if (Decoding(encoded) != decoded) {
     throw TestFailure("Decoding failed");
