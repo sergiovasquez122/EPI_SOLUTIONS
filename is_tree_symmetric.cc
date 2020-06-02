0173d4b72f27841a1f9fe7e6541e70239b667df1
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-01 20:33:31 -0700

    added solution

--- is_tree_symmetric.cc
+++ is_tree_symmetric.cc
@@ -1,9 +1,14 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 
+bool helper(BinaryTreeNode<int>* p1, BinaryTreeNode<int>* p2){
+  if(!p1 && !p2) return true;
+  if((p1 && !p2) || !p1) return false;
+  return p1->data == p2->data && helper(p1->left.get(), p2->right.get()) && helper(p1->right.get(), p2->left.get());
+}
+
 bool IsSymmetric(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+    return helper(tree.get(), tree.get());
 }
 
 int main(int argc, char* argv[]) {
