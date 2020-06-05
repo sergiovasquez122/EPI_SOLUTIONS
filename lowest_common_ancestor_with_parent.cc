a1a3728f9bd83c49557578dcafea7e977b86b392
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-05 15:26:57 -0700

    added solution

--- lowest_common_ancestor_with_parent.cc
+++ lowest_common_ancestor_with_parent.cc
@@ -6,8 +6,36 @@
 
 BinaryTreeNode<int>* Lca(const unique_ptr<BinaryTreeNode<int>>& node0,
                          const unique_ptr<BinaryTreeNode<int>>& node1) {
-  // TODO - you fill in here.
-  return nullptr;
+    int height_1 = 0, height_2 = 0;
+    auto iter1 = node0.get();
+    while(iter1){
+      iter1 = iter1->parent;
+      height_1++;
+    }
+
+    auto iter2 = node1.get();
+    while(iter2){
+        iter2 = iter2->parent;
+        height_2++;
+    }
+
+    int diff = abs(height_2 - height_1);
+
+    iter1 = height_1 < height_2 ? node1.get() : node0.get();
+    while(diff--){
+        iter1 = iter1->parent;
+    }
+
+    iter2 = height_1 < height_2 ? node0.get() : node1.get();
+    while(iter1 && iter2){
+        if(iter1 == iter2){
+            return iter1;
+        }
+        iter1 = iter1->parent;
+        iter2 = iter2->parent;
+    }
+
+    return iter1;
 }
 int LcaWrapper(TimedExecutor& executor,
                const unique_ptr<BinaryTreeNode<int>>& tree, int key0,
