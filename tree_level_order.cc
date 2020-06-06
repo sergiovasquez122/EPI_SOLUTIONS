23112af764920adf46c58f458758f984caa981ad
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-06 12:58:50 -0700

    added solution

--- tree_level_order.cc
+++ tree_level_order.cc
@@ -1,15 +1,36 @@
 #include <memory>
 #include <vector>
-
+#include <queue>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 using std::unique_ptr;
 using std::vector;
+using std::queue;
 
 vector<vector<int>> BinaryTreeDepthOrder(
     const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+    vector<vector<int>> result;
+    queue<BinaryTreeNode<int>*> curr_depth_nodes({tree.get()});
+    while(!curr_depth_nodes.empty()){
+        vector<int> this_level;
+        queue<BinaryTreeNode<int>*> next_level_nodes;
+        while(!curr_depth_nodes.empty()){
+            auto curr = curr_depth_nodes.front();
+            curr_depth_nodes.pop();
+
+            if(curr){
+                this_level.push_back(curr->data);
+
+                next_level_nodes.push(curr->left.get());
+                next_level_nodes.push(curr->right.get());
+            }
+        }
+        if(!this_level.empty()){
+            result.push_back(this_level);
+        }
+        std::swap(curr_depth_nodes, next_level_nodes);
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
