ee8f354bc4e17586b51935f964cd63f117abda4e
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-06 10:49:12 -0700

    added solution

--- snake_string.cc
+++ snake_string.cc
@@ -4,7 +4,21 @@
 using std::string;
 
 string SnakeString(const string& s) {
-    return "";
+    string result;
+
+    for(int i = 1;i < s.size();i+=4){
+        result += s[i];
+    }
+
+    for(int i = 0;i < s.size();i+=2){
+        result += s[i];
+    }
+
+    for(int i = 3;i < s.size();i += 4){
+        result += s[i];
+    }
+
+    return result;
 }
 
 int main(int argc, char* argv[]) {
