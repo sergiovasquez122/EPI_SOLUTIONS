dc54a34ec6ab6062f7a3f29deef67b5fda345841
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-07 13:24:29 -0700

    added solution

--- two_sum.cc
+++ two_sum.cc
@@ -1,10 +1,21 @@
 #include <vector>
-
+#include <unordered_map>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::unordered_set;
+
 bool HasTwoSum(const vector<int>& A, int t) {
-  // TODO - you fill in here.
-  return true;
+  unordered_set<int> elements;
+  for(int x : A){
+    elements.insert(x);
+  }
+  
+  for(int y : A){
+    if(elements.count(t - y)){
+      return true;
+    }
+  }
+  return false;
 }
 
 int main(int argc, char* argv[]) {
