42b56e88ede49a8467bbadbe6ef1113aba94991a
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-10 14:01:46 -0700

    added solution

--- permutations.cc
+++ permutations.cc
@@ -3,9 +3,23 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
+
+void helper(vector<vector<int>>& result, vector<int>& perm, int begin){
+    if(begin == perm.size() - 1) {
+        result.push_back(perm);
+    } else {
+        for(int i = begin;i < perm.size();++i){
+            std::swap(perm[begin], perm[i]);
+            helper(result, perm, begin + 1);
+            std::swap(perm[begin], perm[i]);
+        }
+    }
+}
+
 vector<vector<int>> Permutations(vector<int> A) {
-  // TODO - you fill in here.
-  return {};
+    vector<vector<int>> result;
+    helper(result, A, 0);
+    return result;
 }
 
 int main(int argc, char* argv[]) {
