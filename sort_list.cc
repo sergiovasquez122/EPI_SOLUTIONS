279f1e6cf959c323d277abbc9c8f678ad6554992
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-11 22:41:54 -0700

    added solution

--- sort_list.cc
+++ sort_list.cc
@@ -3,9 +3,41 @@
 #include "list_node.h"
 #include "test_framework/generic_test.h"
 
+shared_ptr<ListNode<int>> merge(shared_ptr<ListNode<int>> l1, shared_ptr<ListNode<int>> l2){
+    ListNode<int> dummy;
+    auto tail = &dummy;
+    while(l1 && l2){
+        if(l1->data < l2->data){
+            tail->next = l1;
+            l1 = l1->next;
+            tail = tail->next.get();
+        } else {
+            tail->next = l2;
+            l2 = l2->next;
+            tail = tail->next.get();
+        }
+    }
+
+    tail->next = l1 ? l1 : l2;
+    return dummy.next;
+}
+
 shared_ptr<ListNode<int>> StableSortList(shared_ptr<ListNode<int>> L) {
-  // TODO - you fill in here.
-  return nullptr;
+    if(L == nullptr || L->next == nullptr){
+        return L;
+    }
+
+    shared_ptr<ListNode<int>> slow = L, fast = L, pre = nullptr;
+
+    while(fast && fast->next){
+        fast = fast->next->next;
+        pre = slow;
+        slow = slow->next;
+    }
+    pre->next = nullptr;
+    auto lhs = StableSortList(L);
+    auto rhs = StableSortList(slow);
+    return merge(lhs, rhs);
 }
 
 int main(int argc, char* argv[]) {
