9516713007c9fefba4816d7fbe1861f3800344fb
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-11 19:34:47 -0700

    added solution

--- reverse_words.cc
+++ reverse_words.cc
@@ -1,13 +1,21 @@
 #include <string>
-
+#include <algorithm>
 #include "test_framework/generic_test.h"
 #include "test_framework/timed_executor.h"
 using std::string;
+using std::reverse;
 
 void ReverseWords(string* s) {
-  // TODO - you fill in here.
-  return;
+    reverse(s->begin(),s->end());
+    int begin = 0;
+    int end = 0;
+    while((end = s->find(' ', begin)) != string::npos){
+        reverse(s->begin() + begin, s->begin() + end);
+        begin = end + 1;
+    }
+    reverse(s->begin() + begin, s->end());
 }
+
 string ReverseWordsWrapper(TimedExecutor& executor, string s) {
   string s_copy = s;
 
