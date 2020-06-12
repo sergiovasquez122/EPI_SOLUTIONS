1226734d20f240a89298b0803bf31c43a30fe4f9
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-12 12:09:49 -0700

    added solution

--- tree_preorder.cc
+++ tree_preorder.cc
@@ -1,12 +1,23 @@
 #include <vector>
-
+#include <stack>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
 using std::vector;
-
+using std::stack;
 vector<int> PreorderTraversal(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+    stack<const BinaryTreeNode<int>*> on_stack;
+    vector<int> result;
+    on_stack.push(tree.get());
+    while(!on_stack.empty()){
+        auto curr = on_stack.top();
+        on_stack.pop();
+        if(curr){
+            result.push_back(curr->data);
+            on_stack.push(curr->right.get());
+            on_stack.push(curr->left.get());
+        }
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
