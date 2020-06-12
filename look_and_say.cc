d8fc0c55667b26b36782c0a152c2c1cf3e03a9a6
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-11 22:00:53 -0700

    added solution

--- look_and_say.cc
+++ look_and_say.cc
@@ -1,11 +1,24 @@
 #include <string>
-
 #include "test_framework/generic_test.h"
 using std::string;
+using std::to_string;
+
+string iteration(const string& s){
+    string result;
+    for(int i = 0;i < s.size();++i){
+        int counter = 1;
+        while(i + 1 < s.size() && s[i] == s[i + 1]) i++, counter++;
+        result += to_string(counter) + s[i];
+    }
+    return result;
+}
 
 string LookAndSay(int n) {
-  // TODO - you fill in here.
-  return "";
+    string s = "1";
+    for(int i = 1;i < n;++i){
+        s = iteration(s); 
+    }
+    return s;
 }
 
 int main(int argc, char* argv[]) {
