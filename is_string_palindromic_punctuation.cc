02d7beeff0fd5ceb7686f40c0cd39ca360c4cd3d
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-11 22:44:53 -0700

    added solution

--- is_string_palindromic_punctuation.cc
+++ is_string_palindromic_punctuation.cc
@@ -3,7 +3,14 @@
 #include "test_framework/generic_test.h"
 using std::string;
 bool IsPalindrome(const string& s) {
-  // TODO - you fill in here.
+  int i = 0, j = s.size() - 1;
+  while(i < j){
+    while(i < j && !isalnum(s[i])) i++;
+
+    while(j > i && !isalnum(s[j]) ) j--;
+
+    if(tolower(s[i++]) != tolower(s[j--])) return false;
+  }
   return true;
 }
 
