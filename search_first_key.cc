56121148f7d73c39ba5618db1c19242079001eba
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-13 18:40:35 -0700

    added solution

--- search_first_key.cc
+++ search_first_key.cc
@@ -1,11 +1,22 @@
 #include <vector>
-
 #include "test_framework/generic_test.h"
 using std::vector;
 
 int SearchFirstOfK(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return 0;
+    int low = 0, hi = A.size() - 1;
+    int ret = -1;
+    while( low <= hi ){
+        int mid = (low + hi) / 2;
+        if(A[mid] < k) {
+            low = mid + 1;
+        } else if(A[mid] > k){
+            hi = mid - 1;
+        } else {
+            ret = mid;
+            hi = mid - 1;
+        }
+    }
+    return ret;
 }
 
 int main(int argc, char* argv[]) {
