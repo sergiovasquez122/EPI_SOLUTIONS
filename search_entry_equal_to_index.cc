c0296c459f68944e3e4c63826f1ff83fc9cd21d1
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-13 21:57:33 -0700

    added solution

--- search_entry_equal_to_index.cc
+++ search_entry_equal_to_index.cc
@@ -6,8 +6,18 @@
 using std::vector;
 
 int SearchEntryEqualToItsIndex(const vector<int>& A) {
-  // TODO - you fill in here.
-  return 0;
+    int lo = 0, hi = A.size() - 1;
+    while(lo <= hi){
+        int mid = (lo + hi) / 2;
+        if(A[mid] == mid){
+            return mid;
+        } else if(A[mid] < mid){
+            lo = mid + 1;
+        } else {
+            hi = mid - 1;
+        }
+    }
+    return -1;
 }
 void SearchEntryEqualToItsIndexWrapper(TimedExecutor& executor,
                                        const vector<int>& A) {
