2cbcc118526ea64891520b217d03f2a7db29f39a
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-18 15:06:11 -0700

    added solutions

--- tree_exterior.cc
+++ tree_exterior.cc
@@ -5,12 +5,52 @@
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
 using std::vector;
+using std::unique_ptr;
+
+bool isLeaf(const unique_ptr<BinaryTreeNode<int>>& root){
+    return !root->left && !root->right;
+}
+
+void collect_right(const unique_ptr<BinaryTreeNode<int>>& root, vector<const unique_ptr<BinaryTreeNode<int>>*>& result,
+                  bool is_boundary){
+    if(!root){
+        return;
+    }
+
+    collect_right(root->left, result, root->right == nullptr && is_boundary);
+    collect_right(root->right, result, is_boundary);
+
+    if(isLeaf(root) || is_boundary){
+        result.push_back(&root);
+    }
+
+}
+
+void collect_left(const unique_ptr<BinaryTreeNode<int>>& root, vector<const unique_ptr<BinaryTreeNode<int>>*>& result,
+        bool is_boundary){
+        if(!root){
+            return;
+        }
+
+        if(isLeaf(root) || is_boundary){
+            result.push_back(&root);
+        }
+
+        collect_left(root->left, result, is_boundary);
+        collect_left(root->right, result, root->left == nullptr && is_boundary);
+}
 
 vector<const unique_ptr<BinaryTreeNode<int>>*> ExteriorBinaryTree(
     const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+    vector<const unique_ptr<BinaryTreeNode<int>>*> result;
+    if(tree != nullptr){
+        result.push_back(&tree);
+        collect_left(tree->left, result,true);
+        collect_right(tree->right, result, true);
+    }
+    return result;
 }
+
 vector<int> CreateOutputVector(
     const vector<const unique_ptr<BinaryTreeNode<int>>*>& L) {
   if (std::find(std::begin(L), std::end(L), nullptr) != std::end(L)) {
