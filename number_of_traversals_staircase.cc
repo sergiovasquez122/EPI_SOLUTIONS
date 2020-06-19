210c0dfc57e2a3827708c59c8d0ca369629da682
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-18 17:27:54 -0700

    added solution

--- number_of_traversals_staircase.cc
+++ number_of_traversals_staircase.cc
@@ -1,8 +1,19 @@
 #include "test_framework/generic_test.h"
+#include <vector>
+
+using std::vector;
 
 int NumberOfWaysToTop(int top, int maximum_step) {
-  // TODO - you fill in here.
-  return 0;
+  vector<int> memo(top + 1, 0);
+  memo[0] = 1;
+  for(int i = 1;i < memo.size();++i){
+    for(int j = 1;j <= maximum_step;++j){
+        if(i - j >= 0){
+          memo[i] += memo[i - j];
+        }
+    }
+  }
+  return memo.back();
 }
 
 int main(int argc, char* argv[]) {
