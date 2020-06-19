8c8c01770e5fb5e483413042316b4a2c0b3e0f87
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-18 19:08:52 -0700

    added solution

--- minimum_weight_path_in_a_triangle.cc
+++ minimum_weight_path_in_a_triangle.cc
@@ -1,11 +1,26 @@
 #include <vector>
-
+#include <algorithm>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::min;
+using std::min_element;
 
 int MinimumPathWeight(const vector<vector<int>>& triangle) {
-  // TODO - you fill in here.
-  return 0;
+    if(triangle.empty()){
+      return 0;
+    }
+
+    vector<int> current_row(triangle.front());
+    for(int i = 1;i < triangle.size();++i){
+        vector<int> next_row(triangle[i]);
+        next_row.front() += current_row.front();
+        for(int k = 1;k < next_row.size() - 1;++k){
+            next_row[k] += min(current_row[k - 1], current_row[k]);
+        }
+        next_row.back() += current_row.back();
+        std::swap(current_row, next_row);
+    }
+    return *min_element(current_row.begin(), current_row.end());
 }
 
 int main(int argc, char* argv[]) {
