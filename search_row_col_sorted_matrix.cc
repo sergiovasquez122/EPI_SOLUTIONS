4b7ef5cf4f12754648b06af2cf1a770aea85ce60
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-21 15:35:38 -0700

    added solution

--- search_row_col_sorted_matrix.cc
+++ search_row_col_sorted_matrix.cc
@@ -1,11 +1,22 @@
 #include <vector>
-
+#include <algorithm>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::binary_search;
 
 bool MatrixSearch(const vector<vector<int>>& A, int x) {
-  // TODO - you fill in here.
-  return true;
+    int row = 0;
+    int col = A[0].size() - 1;
+    while(row < A.size() && col >= 0){
+        if(A[row][col] == x){
+            return true;
+        } else if(A[row][col] < x){
+            row++;
+        } else {
+            col--;
+        }
+    }
+    return false;
 }
 
 int main(int argc, char* argv[]) {
