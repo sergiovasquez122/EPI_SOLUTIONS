e9155588238d1113e885a143ce3f48447619862d
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-21 12:05:52 -0700

    added solution

--- int_as_array_multiply.cc
+++ int_as_array_multiply.cc
@@ -3,8 +3,22 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> Multiply(vector<int> num1, vector<int> num2) {
-  // TODO - you fill in here.
-  return {};
+    bool isNeg = num1.front() < 0 ^ num2.front() < 0;
+    num1.front() = abs(num1.front());
+    num2.front() = abs(num2.front());
+    vector<int> result(num1.size() + num2.size(), 0);
+    for(int i = num1.size() - 1;i >= 0 ;--i){
+        for(int j = num2.size() - 1;j >= 0; --j){
+            result[i + j + 1] += num1[i] * num2[j];
+            result[i + j] += result[i + j + 1] / 10;
+            result[i + j + 1] %= 10;
+        }
+    }
+    while(result.front() == 0 && result.size() > 1){
+        result.erase(result.begin(), result.begin() + 1);
+    }
+    result.front() = isNeg ? -result.front() : result.front();
+    return result;
 }
 
 int main(int argc, char* argv[]) {
