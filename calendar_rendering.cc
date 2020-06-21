c8197ddb21e7755a95a43abe0d12f72eb66a3a33
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-20 19:29:25 -0700

    added solution

--- calendar_rendering.cc
+++ calendar_rendering.cc
@@ -1,16 +1,44 @@
 #include <vector>
-
+#include <algorithm>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 using std::vector;
-
+using std::sort;
 struct Event {
   int start, finish;
 };
+
+struct endPoint{
+    int point;
+    bool isStart;
+    bool operator <(const endPoint& rhs) const{
+        if(point != rhs.point){
+            return point < rhs.point;
+        }
+        return isStart && !rhs.isStart;
+    }
+};
+
 int FindMaxSimultaneousEvents(const vector<Event>& A) {
-  // TODO - you fill in here.
-  return 0;
+    vector<endPoint> points;
+    for(auto& e : A){
+        points.push_back({e.start, true});
+        points.push_back({e.finish, false});
+    }
+    sort(points.begin(), points.end());
+    
+    int current = 0, maxResult = 0;
+    for(auto & e : points){
+        if(e.isStart){
+            current++;
+            maxResult = std::max(maxResult, current);
+        } else {
+            current--;
+        }
+    }
+    return maxResult;
 }
+
 namespace test_framework {
 template <>
 struct SerializationTrait<Event> : UserSerTrait<Event, int, int> {};
