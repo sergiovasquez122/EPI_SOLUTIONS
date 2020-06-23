0b5dcbf00507b9cc39984d4528b5575bfa87eef4
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-23 11:30:50 -0700

    added solution

--- spiral_ordering.cc
+++ spiral_ordering.cc
@@ -3,8 +3,38 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 vector<int> MatrixInSpiralOrder(const vector<vector<int>>& square_matrix) {
-  // TODO - you fill in here.
-  return {};
+    int L = 0, R = square_matrix.size() - 1;
+    int U = 0, B = square_matrix.size() - 1;
+    int dir = 0;
+    vector<int> result;
+    while(L <= R &&  U <= B){
+        if(dir == 0){
+            for(int i = L;i <= R;++i){
+                result.push_back(square_matrix[U][i]);
+            }
+            ++dir;
+        } else if(dir == 1){
+            for(int i = U + 1; i <= B;++i){
+                result.push_back(square_matrix[i][R]);
+            }
+            ++dir;
+        } else if(dir == 2){
+            for(int i = R - 1;i >= L;--i){
+                result.push_back(square_matrix[B][i]);
+            }
+            ++dir;
+        } else {
+            ++U;
+            for(int i = B - 1;i>=U;--i){
+                result.push_back(square_matrix[i][L]);
+            }
+            dir = 0;
+            --R;
+            ++L;
+            --B;
+        }
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
