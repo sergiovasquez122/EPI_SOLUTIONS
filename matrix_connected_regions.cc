7a78dbe7b039cc4cab1a3569dfb8425effdcceb7
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-25 12:41:15 -0700

    added solution

--- matrix_connected_regions.cc
+++ matrix_connected_regions.cc
@@ -6,10 +6,28 @@
 using std::deque;
 using std::vector;
 
-void FlipColor(int x, int y, vector<deque<bool>>* image_ptr) {
-  // TODO - you fill in here.
-  return;
+bool feasible(vector<deque<bool>>& image, int x, int y, int color){
+  return x >= 0 && x < image.size() && y >= 0 && y < image.size() && image[x][y] == color; 
 }
+
+void dfs(vector<deque<bool>>& image, int x, int y, int color){
+    if(!feasible(image, x, y, color)){
+        return;
+    }
+    image[x][y] = !color;
+    
+    const vector<vector<int>> shifts = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
+    for(auto& shift : shifts){
+        dfs(image, x + shift[0], y + shift[1], color);
+    }
+}
+
+void FlipColor(int x, int y, vector<deque<bool>>* image_ptr) {
+    vector<deque<bool>>& image = *image_ptr;
+    dfs(image, x, y, image[x][y]);
+}
+
 vector<vector<int>> FlipColorWrapper(TimedExecutor& executor, int x, int y,
                                      vector<vector<int>> image) {
   vector<deque<bool>> b;
