d6c9bf665d1af319aea1d725d992a1c465ad6203
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-27 19:48:51 -0700

    added solution

--- is_circuit_wirable.cc
+++ is_circuit_wirable.cc
@@ -1,20 +1,43 @@
 #include <stdexcept>
 #include <vector>
-
+#include <queue>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include "test_framework/timed_executor.h"
 using std::vector;
-
+using std::queue;
 struct GraphVertex {
   int d = -1;
   vector<GraphVertex*> edges;
 };
 
-bool IsAnyPlacementFeasible(vector<GraphVertex>* graph) {
-  // TODO - you fill in here.
+bool bfs(GraphVertex* graph){
+  graph->d = 0;
+  queue<GraphVertex*> on_queue;
+  on_queue.push(graph);
+  while(!on_queue.empty()){
+      for(auto& v : on_queue.front()->edges){
+        if(v->d == -1){
+          v->d = on_queue.front()->d + 1;
+          on_queue.emplace(v);
+        } else if(v->d == on_queue.front()->d){
+          return false;
+        }
+      }
+      on_queue.pop();
+  }
   return true;
 }
+
+bool IsAnyPlacementFeasible(vector<GraphVertex>* graph) {
+    for(GraphVertex& v : *graph){
+      if(v.d == -1 && !bfs(&v)){
+          return false;
+      }
+    }
+    return true;
+}
+
 struct Edge {
   int from;
   int to;
