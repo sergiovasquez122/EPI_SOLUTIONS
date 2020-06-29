25fd59fbf5f04b99a7371099c840c496d095a6a0
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-28 21:42:53 -0700

    added solution

--- sort_almost_sorted_array.cc
+++ sort_almost_sorted_array.cc
@@ -1,12 +1,26 @@
 #include <vector>
-
+#include <queue>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::priority_queue;
+
 vector<int> SortApproximatelySortedData(
     vector<int>::const_iterator sequence_begin,
     const vector<int>::const_iterator& sequence_end, int k) {
-  // TODO - you fill in here.
-  return {};
+  priority_queue<int, vector<int>, std::greater<>> pq;
+  vector<int> result;
+  while(sequence_begin != sequence_end){
+    pq.push(*sequence_begin++);
+    if(pq.size() > k){
+      result.push_back(pq.top());
+      pq.pop();
+    }
+  }
+  while(!pq.empty()){
+    result.push_back(pq.top());
+    pq.pop();
+  }
+  return result;
 }
 vector<int> SortApproximatelySortedDataWrapper(const vector<int>& sequence,
                                                int k) {
