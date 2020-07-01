6029bc40e51c52f6d7761f1c3808178485888bd5
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-06-30 22:08:36 -0700

    added solution

--- is_tree_balanced.cc
+++ is_tree_balanced.cc
@@ -1,8 +1,33 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+
+struct Node{
+    int height;
+    bool balanced;
+};
+
+Node helper(const unique_ptr<BinaryTreeNode<int>>& tree){
+    if(tree == nullptr){
+        return {-1, true};
+    }
+
+    auto left = helper(tree->left);
+    if(!left.balanced){
+        return {0, false};
+    }
+
+    auto right = helper(tree->right);
+    if(!right.balanced){
+        return {0, false};
+    }
+
+    int height = 1 + std::max(left.height, right.height);
+
+    return {height, abs(left.height - right.height) <= 1};
+}
+
 bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+    return helper(tree).balanced;
 }
 
 int main(int argc, char* argv[]) {
