5750bdf32e8cfd93ccb8279641668efc7ebe5af3
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-04 11:25:12 -0700

    added solution

--- sunset_view.cc
+++ sunset_view.cc
@@ -1,14 +1,37 @@
 #include <iterator>
 #include <vector>
-
+#include <stack>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::stack;
+
+struct building{
+    int height, idx;
+};
+
 vector<int> ExamineBuildingsWithSunset(
     vector<int>::const_iterator sequence_begin,
     const vector<int>::const_iterator& sequence_end) {
-  // TODO - you fill in here.
-  return {};
+    vector<int> result;
+    stack<building> candidates;
+    int idx = 0;
+    while(sequence_begin != sequence_end){
+        if(candidates.empty()){
+            candidates.push({*sequence_begin++, idx++});
+        } else{
+            while(!candidates.empty() && candidates.top().height <= *sequence_begin){
+                candidates.pop();
+            }
+            candidates.push({*sequence_begin++, idx++});
+        }
+    }
+    while(!candidates.empty()){
+        result.push_back(candidates.top().idx);
+        candidates.pop();
+    }
+    return result;
 }
+
 vector<int> ExamineBuildingsWithSunsetWrapper(const vector<int>& sequence) {
   return ExamineBuildingsWithSunset(cbegin(sequence), cend(sequence));
 }
