d29fde81db829be558835ae9f34ff141cac63645
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-06 13:05:06 -0700

    added solution

--- enumerate_balanced_parentheses.cc
+++ enumerate_balanced_parentheses.cc
@@ -1,13 +1,34 @@
 #include <string>
 #include <vector>
-
 #include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
 
+void helper(vector<string>& result, string& partial, int n, int left_paren, int right_paren){
+    if(partial.size() == n * 2){
+        result.push_back(partial);
+        return;
+    }
+
+    if(left_paren < n){
+        partial.push_back('(');
+        helper(result, partial, n, left_paren + 1, right_paren);
+        partial.pop_back();
+    }
+
+    if(right_paren < left_paren){
+        partial.push_back(')');
+        helper(result, partial, n, left_paren, right_paren + 1);
+        partial.pop_back();
+    }
+
+}
+
 vector<string> GenerateBalancedParentheses(int num_pairs) {
-  // TODO - you fill in here.
-  return {};
+    vector<string> result;
+    string partial;
+    helper(result, partial, num_pairs, 0, 0);
+    return result;
 }
 
 int main(int argc, char* argv[]) {
