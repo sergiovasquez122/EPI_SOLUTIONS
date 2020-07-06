26afdfaaeccfb2fd0dfe774f40bb3195e0388894
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-06 11:24:00 -0700

    added solution

--- collatz_checker.cc
+++ collatz_checker.cc
@@ -1,11 +1,32 @@
 #include <vector>
-
+#include <unordered_set>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::unordered_set;
 
 bool TestCollatzConjecture(int n) {
-  // TODO - you fill in here.
-  return false;
+    unordered_set<long>  verified_numbers = {1, 2};
+    for(int i = 3;i <= n;i+=2){
+        unordered_set<long> sequence;
+        long val = i;
+        while(val >= 1){
+            if(sequence.insert(val).second == false){
+                return true;
+            }
+            if(val % 2){
+                if(verified_numbers.insert(val).second == false){
+                    break;
+                }
+                if(val < i){
+                    return false;
+                }
+                val = 3 * val + 1;
+            } else {
+                val /= 2;
+            }
+        }
+    }
+    return true;
 }
 
 int main(int argc, char* argv[]) {
