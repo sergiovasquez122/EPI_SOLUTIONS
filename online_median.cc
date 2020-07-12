e4b9a75e38d2ebbdecd2e8ef9e166b853769a681
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-11 18:12:41 -0700

    refactored solution

--- online_median.cc
+++ online_median.cc
@@ -10,17 +10,17 @@ vector<double> OnlineMedian(vector<int>::const_iterator sequence_begin,
     priority_queue<int, vector<int>, std::less<>> max_pq;
     priority_queue<int, vector<int>, std::greater<>> min_pq;
     while(sequence_begin != sequence_end){
-        min_pq.push(*sequence_begin++);
-        
-        max_pq.push(min_pq.top());
-        min_pq.pop();
-        
-        if(min_pq.size() < max_pq.size()){
-            min_pq.push(max_pq.top());
-            max_pq.pop();
-        } 
-        double value = max_pq.size() == min_pq.size() ? (min_pq.top() + max_pq.top()) * .5 : min_pq.top();
+        max_pq.push(*sequence_begin++);
+        min_pq.push(max_pq.top());
+        max_pq.pop();
+        if(max_pq.size() < min_pq.size()){
+            max_pq.push(min_pq.top());
+            min_pq.pop();
+        }
+
+        double value = (max_pq.size() == min_pq.size()) ? 0.5 * (min_pq.top() + max_pq.top()) : max_pq.top();
         result.push_back(value);
+
     }
     return result;
 }
