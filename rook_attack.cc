6006136f3bfa7dac85f76cfd3e871d8586749bba
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-18 13:43:30 -0700

    added solution

--- rook_attack.cc
+++ rook_attack.cc
@@ -4,9 +4,61 @@
 using std::vector;
 
 void RookAttack(vector<vector<int>>* A_ptr) {
-  // TODO - you fill in here.
-  return;
+    vector<vector<int>>& A = *A_ptr;
+
+    bool first_row = false;
+    for(int col = 0;col < A[0].size();++col){
+        if(!A[0][col]){
+            first_row = true;
+            break;
+        }
+    }
+    bool first_col = false;
+    for(int row = 0;row < A.size();++row){
+        if(!A[row][0]){
+            first_col = true;
+            break;
+        }
+    }
+
+    for(int row = 1;row < A.size(); ++row){
+        for(int col = 1;col < A[0].size();++col){
+            if(!A[row][col]){
+                A[0][col] = false;
+                A[row][0] = false;
+            }
+        }
+    }
+
+    for(int row = 1;row < A.size();++row){
+        if(!A[row][0]){
+            for(int col = 1;col < A[0].size();++col){
+                A[row][col] = false;
+            }
+        }
+    }
+
+    for(int col = 1;col < A[0].size();++col){
+        if(!A[0][col]){
+            for(int row = 1;row < A.size();++row){
+                A[row][col] = false;
+            }
+        }
+    }
+
+    if(first_row){
+        for(int col = 0;col < A[0].size();++col){
+            A[0][col] = false;
+        }
+    }
+
+    if(first_col){
+        for(int row = 0;row < A.size();++row){
+            A[row][0] = false;
+        }
+    }
 }
+
 vector<vector<int>> RookAttackWrapper(vector<vector<int>> A) {
   vector<vector<int>> a_copy = A;
   RookAttack(&a_copy);
