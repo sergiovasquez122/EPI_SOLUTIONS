2b4e92c0b594929b3d5d259aaddd932947a43565
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-18 11:52:28 -0700

    added solution

--- string_decompositions_into_dictionary_words.cc
+++ string_decompositions_into_dictionary_words.cc
@@ -1,13 +1,45 @@
 #include <string>
 #include <vector>
-
+#include <unordered_map>
 #include "test_framework/generic_test.h"
 using std::string;
 using std::vector;
+using std::unordered_map;
 
+bool isValid(const string& s, const unordered_map<string,int>& freq_count, int unit_size){
+   unordered_map<string, int> curr_count;
+   for(int i = 0;i * unit_size < s.length();++i){
+       string temp = s.substr(i * unit_size, unit_size);
+       if(!freq_count.count(temp)){
+           return false;
+       }
+       ++curr_count[temp];
+       if(curr_count[temp] > freq_count.at(temp)){
+           return false;
+       }
+   }
+   return true;
+}
+
+// The time complexity is O(N*n*m)
+// where N is the sentence length
+// m is the number of words
+// n is the length of each word
 vector<int> FindAllSubstrings(const string& s, const vector<string>& words) {
-  // TODO - you fill in here.
-  return {};
+    unordered_map<string, int> freq_count;
+    for(auto& w : words){
+        ++freq_count[w];
+    }
+    int unit_size = words.front().size();
+    int length = words.size() * unit_size;
+    vector<int> result;
+    for(int i = 0;i + length <= s.length();++i){
+        string temp = s.substr(i, length);
+        if(isValid(temp, freq_count, unit_size)){
+            result.push_back(i);
+        }
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
