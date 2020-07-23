f82a746def252b3694e3d103445d9fd2ce5b5b15
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-07-23 14:14:44 -0700

    added solution

--- enumerate_palindromic_decompositions.cc
+++ enumerate_palindromic_decompositions.cc
@@ -7,7 +7,34 @@
 using std::string;
 using std::vector;
 
+bool isPalindrome(const string& s){
+    int i = 0, l = s.size() - 1;
+    while(i < l){
+      if(s[i++] != s[l--]) return false;
+    }
+    return true;
+}
+
+void helper(vector<vector<string>>& result, vector<string>& partial, const string& text, int begin){
+    if(begin == text.size()){
+        result.push_back(partial);
+        return;
+    }
+    for(int i = begin + 1;i <= text.size();++i){
+        string s = text.substr(begin, i - begin);
+        if(isPalindrome(s)){
+            partial.push_back(s);
+            helper(result, partial, text, i);
+            partial.pop_back();
+        }
+    }
+}
+
 vector<vector<string>> PalindromeDecompositions(const string& text) {
+    vector<vector<string>> result;
+    vector<string> partial;
+    helper(result, partial, text, 0);
+    return result;
 }
 
 bool Comp(vector<vector<string>> expected, vector<vector<string>> result) {
