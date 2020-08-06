a4459f1ab2a103f6c15e0c405a04200a5291d587
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-06 11:29:29 -0700

    started implementation of RomanToInteger

--- roman_to_integer.cc
+++ roman_to_integer.cc
@@ -1,10 +1,14 @@
 #include <string>
-
+#include <unordered_map>
 #include "test_framework/generic_test.h"
+
 using std::string;
+using std::unordered_map;
+
 int RomanToInteger(const string& s) {
-  // TODO - you fill in here.
-  return 0;
+    if(s.empty()) return 0;
+    unordered_map<char, int> roman_to_int_mapping = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
+    int result = 0;
 }
 
 int main(int argc, char* argv[]) {
