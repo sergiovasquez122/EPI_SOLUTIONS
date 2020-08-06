17686990d7c4ff7f11b6b2c207d5c641162b8a8f
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-06 11:36:13 -0700

    added solution

--- roman_to_integer.cc
+++ roman_to_integer.cc
@@ -5,10 +5,22 @@
 using std::string;
 using std::unordered_map;
 
+/**
+ * Solution is O(n) as we walk through the entire input
+ * The space is constant
+ */
 int RomanToInteger(const string& s) {
     if(s.empty()) return 0;
     unordered_map<char, int> roman_to_int_mapping = {{'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}};
-    int result = 0;
+    int result = roman_to_int_mapping[s.back()];
+    for(int i = s.size() - 2;i >= 0;i--){
+        if(roman_to_int_mapping[s[i]] < roman_to_int_mapping[s[i + 1]]){
+            result -= roman_to_int_mapping[s[i]];
+        } else {
+            result += roman_to_int_mapping[s[i]];
+        }
+    }
+    return result;
 }
 
 int main(int argc, char* argv[]) {
