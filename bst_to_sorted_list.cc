e0edbecebbab249c5e55c9d753e1efcc8a8cd5f5
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-07 16:49:02 -0700

    added solution

--- bst_to_sorted_list.cc
+++ bst_to_sorted_list.cc
@@ -1,24 +1,39 @@
 #include <memory>
 #include <vector>
-
+#include <stack>
 #include "bst_prototype_shared_ptr.h"
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
-using std::shared_ptr;
 
-struct head_to_tail{
-    shared_ptr<BstNode<int>> head, tail;
-};
 
-head_to_tail helper(const shared_ptr<BstNode<int>>& tree){
-}
+using namespace std;
 
 shared_ptr<BstNode<int>> BSTToDoublyLinkedList(
     const shared_ptr<BstNode<int>>& tree) {
-  // TODO - you fill in here.
-  return nullptr;
+    stack<shared_ptr<BstNode<int>>> on_stack;
+    auto iter = tree;
+    auto pre = iter;
+    pre = nullptr;
+    auto result = pre;
+    while(iter || !on_stack.empty()){
+        while(iter){
+            on_stack.push(iter);
+            iter = iter->left;
+        }
+        iter = on_stack.top();
+        on_stack.pop();
+        if(!pre) result = iter;
+        if(pre){
+            pre->right = iter;
+            iter->left = pre;
+        }
+        pre = iter;
+        iter = iter->right;
+    }
+    return result;
 }
+
 std::vector<int> BSTToDoublyLinkedListWrapper(
     TimedExecutor& executor, const std::shared_ptr<BstNode<int>>& tree) {
   auto list = executor.Run([&] { return BSTToDoublyLinkedList(tree); });
