372ad204c06494d32b902bab4bd76b7e578cda0f
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-06 18:46:26 -0700

    added solution

--- power_x_y.cc
+++ power_x_y.cc
@@ -1,7 +1,18 @@
 #include "test_framework/generic_test.h"
+double helper(double x, int y){
+    if(y == 1) return x;
+    double c = x * x;
+    double ans = helper(c, y / 2);
+    if(y % 2 == 1) ans = ans * x;
+    return ans;
+}
 double Power(double x, int y) {
-  // TODO - you fill in here.
-  return 0.0;
+    if(y == 0) return 1;
+    if(y < 0) {
+        x = 1 / x;
+        y = -y;
+    }
+    return helper(x, y);
 }
 
 int main(int argc, char* argv[]) {
