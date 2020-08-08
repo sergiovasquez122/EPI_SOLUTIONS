09e01fc5422f477f348c9f82d14bac55636350f3
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-07 17:57:51 -0700

    started implementation of bst_merge

--- bst_merge.cc
+++ bst_merge.cc
@@ -1,13 +1,42 @@
 #include <memory>
-
+#include <stack>
 #include "bst_prototype_shared_ptr.h"
 #include "test_framework/generic_test.h"
 using std::shared_ptr;
+using std::stack;
+
+shared_ptr<BstNode<int>> BSTToDoublyLinkedList(
+        const shared_ptr<BstNode<int>>& tree) {
+  stack<shared_ptr<BstNode<int>>> on_stack;
+  auto iter = tree;
+  auto pre = iter;
+  pre = nullptr;
+  auto result = pre;
+  while(iter || !on_stack.empty()){
+    while(iter){
+      on_stack.push(iter);
+      iter = iter->left;
+    }
+    iter = on_stack.top();
+    on_stack.pop();
+    if(!pre) result = iter;
+    if(pre){
+      pre->right = iter;
+      iter->left = pre;
+    }
+    pre = iter;
+    iter = iter->right;
+  }
+  return result;
+}
 
 shared_ptr<BstNode<int>> MergeTwoBSTs(shared_ptr<BstNode<int>> A,
                                       shared_ptr<BstNode<int>> B) {
-  // TODO - you fill in here.
-  return nullptr;
+    auto lhs = BSTToDoublyLinkedList(A); // O(n)
+    auto rhs = BSTToDoublyLinkedList(B); // O(n)
+    // merge items
+    // count length
+    return
 }
 
 int main(int argc, char* argv[]) {
