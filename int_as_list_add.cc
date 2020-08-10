4569c32a567115bac7e3970ea06885de85ad6f08
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-09 19:46:27 -0700

    added solution

--- int_as_list_add.cc
+++ int_as_list_add.cc
@@ -3,8 +3,26 @@
 
 shared_ptr<ListNode<int>> AddTwoNumbers(shared_ptr<ListNode<int>> L1,
                                         shared_ptr<ListNode<int>> L2) {
-  // TODO - you fill in here.
-  return nullptr;
+    int carry = 0;
+    shared_ptr<ListNode<int>> dummy = shared_ptr<ListNode<int>>(new ListNode<int>(0));
+    auto tail = dummy;
+    while(L1 || L2 || carry){
+        int sum = 0;
+        if(L1){
+            sum += L1->data;
+            L1 = L1->next;
+        }
+        if (L2){
+            sum += L2->data;
+            L2 = L2->next;
+        }
+        sum += carry;
+        carry = sum / 10;
+        sum %= 10;
+        tail->next = make_shared<ListNode<int>>(ListNode<int>(sum));
+        tail = tail->next;
+    }
+    return dummy->next;
 }
 
 int main(int argc, char* argv[]) {
