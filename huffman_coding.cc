cf7e5a88b66bc49170015591a6bc6d4e01e6b171
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-14 18:23:59 -0700

    partial implementation of huffman encoding

--- huffman_coding.cc
+++ huffman_coding.cc
@@ -1,20 +1,46 @@
 #include <string>
 #include <vector>
-
+#include <memory>
+#include <queue>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 using std::string;
 using std::vector;
+using std::shared_ptr;
+using std::priority_queue;
+
 namespace huffman {
 
 struct CharWithFrequency {
   char c;
   double freq;
 };
+
+struct BinaryTree{
+    double aggregate_freq;
+    CharWithFrequency* s;
+    shared_ptr<BinaryTree> left, right;
+};
+
+struct FrequencyComparator{
+    bool operator()(const shared_ptr<BinaryTree>& lhs, const shared_ptr<BinaryTree>& rhs){
+        return lhs->aggregate_freq < rhs->aggregate_freq;
+    }
+};
+
 double HuffmanEncoding(vector<CharWithFrequency>* symbols) {
-  // TODO - you fill in here.
+  priority_queue<shared_ptr<BinaryTree>, vector<shared_ptr<BinaryTree>>, FrequencyComparator> minpq;
+  while(minpq.size() > 1){
+      auto left = minpq.top();
+      minpq.pop();
+      auto right = minpq.top();
+      minpq.pop();
+      minpq.emplace(new BinaryTree{left->aggregate_freq + right->aggregate_freq, nullptr, left, right});
+  }
+
   return 0.0;
 }
+
 }  // namespace huffman
 namespace test_framework {
 template <>
