6e39cf2a981d34516fd1037d0ce3c65d0ebb4133
Sergio Vasquez <sergiovasquez122@gmail.com>
2020-08-14 18:33:50 -0700

    added huffman_coding

--- huffman_coding.cc
+++ huffman_coding.cc
@@ -2,12 +2,16 @@
 #include <vector>
 #include <memory>
 #include <queue>
+#include <unordered_map>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 using std::string;
 using std::vector;
 using std::shared_ptr;
 using std::priority_queue;
+using std::unordered_map;
+using std::unique_ptr;
+using std::make_unique;
 
 namespace huffman {
 
@@ -24,12 +28,30 @@ struct BinaryTree{
 
 struct FrequencyComparator{
     bool operator()(const shared_ptr<BinaryTree>& lhs, const shared_ptr<BinaryTree>& rhs){
-        return lhs->aggregate_freq < rhs->aggregate_freq;
+        return lhs->aggregate_freq > rhs->aggregate_freq;
     }
 };
 
+void assignHuffmanCode(const shared_ptr<BinaryTree>& tree, const unique_ptr<string>& code, unordered_map<char, string>* huffman_encoding){
+    if(tree){
+        if(tree->s){
+            (*huffman_encoding)[tree->s->c] = *code;
+        } else{
+            code->push_back('0');
+            assignHuffmanCode(tree->left, code, huffman_encoding);
+            code->back() = '1';
+            assignHuffmanCode(tree->right, code, huffman_encoding);
+            code->pop_back();
+        }
+    }
+
+}
+
 double HuffmanEncoding(vector<CharWithFrequency>* symbols) {
   priority_queue<shared_ptr<BinaryTree>, vector<shared_ptr<BinaryTree>>, FrequencyComparator> minpq;
+  for(auto& s : *symbols){
+      minpq.emplace(new BinaryTree{s.freq, &s, nullptr, nullptr});
+  }
   while(minpq.size() > 1){
       auto left = minpq.top();
       minpq.pop();
@@ -37,8 +59,13 @@ double HuffmanEncoding(vector<CharWithFrequency>* symbols) {
       minpq.pop();
       minpq.emplace(new BinaryTree{left->aggregate_freq + right->aggregate_freq, nullptr, left, right});
   }
-
-  return 0.0;
+  unordered_map<char, string> huffman_encoding;
+  assignHuffmanCode(minpq.top(), make_unique<string>(), &huffman_encoding);
+  double avg = 0;
+  for(const auto& s : *symbols){
+      avg += huffman_encoding[s.c].size() * s.freq  / 100;
+  }
+  return avg;
 }
 
 }  // namespace huffman
